d53f412e424cb95dacaff6e0b5b1a65d3b45b32d
AbonentVneSeti <148952123+AbonentVneSeti@users.noreply.github.com>
2025-04-20 14:50:43 +0200

    +Lab 1.2

---
+++ Lab 1/1.1/1.1.py
@@ -0,0 +1,151 @@
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+def read_csv(file_path : str) -> list[list]:
+    data = list()
+
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        data.append(next(reader))
+        for row in reader:
+            data.append(list())
+            for i in row:
+                data[-1].append(float(i))
+    return data
+
+def stat_analyisis(data : list[list])-> dict:
+    numvalues = len(data) - 1
+    minvalues = dict()
+    maxvalues = dict()
+    averagevalues = dict()
+
+    headers = data[0]
+
+    for i in headers:
+        minvalues[i] = 9999999.0
+        maxvalues[i] = -9999999.0
+        averagevalues[i] = 0
+
+
+    for row in data[1:]:
+        for i in range(len(headers)):
+            if row[i] < minvalues[headers[i]]:
+                minvalues[headers[i]] = row[i]
+            if row[i] > maxvalues[headers[i]]:
+                maxvalues[headers[i]] = row[i]
+            averagevalues[headers[i]] += row[i]
+
+    for i in headers:
+        averagevalues[i]/= numvalues
+
+    stats = {"numvalues": numvalues, "minvalues": minvalues, "maxvalues": maxvalues, "averagevalues": averagevalues}
+    return stats
+
+def calculate_coefficients(data : list[list],x_ind : int,y_ind : int) -> list[float]:
+    n = len(data)-1
+    y_sum = 0
+    x_sum = 0
+    square_x_sum = 0
+    x_mult_y_sum = 0
+    for i in data[1:]:
+        y_sum+=i[y_ind]
+        x_sum+=i[x_ind]
+        x_mult_y_sum += i[x_ind]*i[y_ind]
+        square_x_sum += i[x_ind]*i[x_ind]
+
+    w1 = ( (x_sum*y_sum)/n - x_mult_y_sum )/( (x_sum*x_sum)/n - square_x_sum )
+
+    w0 = y_sum/n - (w1 * x_sum)/n
+
+    return[w0,w1]
+
+def func(x : float, line_coeffs : list) -> float:
+    return line_coeffs[0] + x*line_coeffs[1]
+
+def inv_func(y : float, line_coeffs : list) -> float:
+    return (y - line_coeffs[0])/line_coeffs[1]
+
+def get_rects(x : list ,y : list, line_coeffs : list)-> list[Rectangle]:
+    rects = list()
+    #rects.append(Rectangle((10,10),20,20,edgecolor = 'black',facecolor = 'None'))
+
+    for i in range(len(x)):
+        point = (x[i],y[i])
+        delta_y = point[1] - func(point[0],line_coeffs)
+        delta_x = abs(point[0] - inv_func(point[1],line_coeffs))
+        if delta_y>0:
+            rects.append(Rectangle(point,delta_x,-delta_y, edgecolor = 'black', hatch = '/', facecolor = 'None'))
+        elif delta_y < 0:
+            rects.append(Rectangle(point,-delta_x,-delta_y, edgecolor = 'black', hatch = '\\', facecolor = 'None'))
+
+    return rects
+
+def draw(x : list ,y : list, line_coeffs : list, x_axis = "X", y_axis = "Y") -> None:
+    plt.figure(figsize = (16,8))
+    plt.subplots_adjust(hspace=0.3)
+
+    plt.subplot(2,2,1)
+    plt.scatter(x, y)
+    plt.title("Все точки")
+    ax = plt.gca()
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+
+    plt.subplot(2,2,2)
+    plt.scatter(x, y)
+    plt.axline((1,line_coeffs[0]+line_coeffs[1]*1),(10,line_coeffs[0]+line_coeffs[1]*10),color = 'red')
+    plt.title("Линейная регрессия")
+    ax = plt.gca()
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+
+    plt.subplot(2,2,3)
+    plt.scatter(x, y)
+    plt.axline((1, func(1,line_coeffs)), (10, func(10,line_coeffs)), color = 'red')
+    for i in get_rects(x,y, line_coeffs):
+        plt.gca().add_patch(i)
+    plt.title("Линейная регрессия с квадратами MSE")
+    ax = plt.gca()
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+
+    plt.show()
+
+def main():
+    data = read_csv("student_scores.csv")
+    print("Считанные данные:\n",data,sep='')
+
+    stats = stat_analyisis(data)
+    print("Статистика:\n",stats,sep='')
+
+    x_ind = 0
+    y_ind = 0
+
+    print("\nДоступные столбцы:")
+    for i in range( len(data[0])):
+        print(f"{i} : {data[0][i]}")
+
+    input_flag = True
+    while input_flag:
+        x_ind = int(input("Выберите, какой столбец будет отвечать за x: "))
+
+        y_ind = int(input("Выберите, какой столбец будет отвечать за y: "))
+
+        if x_ind == y_ind or x_ind > len(data[0]) or x_ind < 0 or y_ind > len(data[0]) or y_ind < 0:
+            print("Ввод некорректен, повторите попытку.")
+        else:
+            print("Принято!")
+            input_flag = False
+
+    x = [i[x_ind] for i in data[1:]]
+    y = [i[y_ind] for i in data[1:]]
+
+    coeffs = calculate_coefficients(data,x_ind,y_ind)
+    print(f"Искомая функция: y = {coeffs[0]} + {coeffs[1]}*x")
+
+
+    draw(x,y,coeffs,data[0][x_ind],data[0][y_ind])
+
+if __name__ == "__main__":
+    main()
---
+++ Lab 1/1.2/1.2.py
@@ -0,0 +1,86 @@
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def my_lin_calculate_coefficients(data, x_ind, y_ind):
+    n = len(data)-1
+    y_sum = 0
+    x_sum = 0
+    square_x_sum = 0
+    x_mult_y_sum = 0
+    for i in data[1:]:
+        y_sum+=i[y_ind]
+        x_sum+=i[x_ind]
+        x_mult_y_sum += i[x_ind]*i[y_ind]
+        square_x_sum += i[x_ind]*i[x_ind]
+
+    w1 = ( (x_sum*y_sum)/n - x_mult_y_sum )/( (x_sum*x_sum)/n - square_x_sum )
+
+    w0 = y_sum/n - (w1 * x_sum)/n
+
+    return[w0,w1]
+
+def my_lin_regression(x_train, y_train, x_test):
+    data= list()
+    data.append([None,None])
+    for i in range(len(x_train)):
+        data.append([x_train[i], y_train[i]])
+
+    coeffs = my_lin_calculate_coefficients(data,0,1)
+
+    my_lin_predictions = list()
+    for x in x_test:
+        my_lin_predictions.append((coeffs[0] + coeffs[1]*x)[0])
+    return [my_lin_predictions,coeffs]
+
+def main():
+    diabetes = datasets.load_diabetes()
+
+    df = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
+    df['target'] = diabetes.target  # уровень глюкозы
+    x = df[['bmi']].values
+    y = df['target'].values
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=5)
+
+    #Модель sklearn
+    sl_reg = LinearRegression()
+    sl_reg.fit(x_train, y_train)
+    sl_predictions = sl_reg.predict(x_test)
+
+    #Собственный алгоритм
+    my_reg = my_lin_regression(x_train,y_train,x_test)
+    my_lin_coeffs = my_reg[1]
+    my_lin_predictions = my_reg[0]
+
+
+    #Выводы
+    print("Resulting model functions")
+    print(f"sklearn: {sl_reg.intercept_:.2f} + x * {sl_reg.coef_[0]:.2f}")
+    print(f"my lin reg: {my_lin_coeffs[0][0]:.2f} + x * {my_lin_coeffs[1][0]:.2f}")
+    print()
+    n = 30
+    print(f"Prediction table, first {n} pieces:")
+    print('x    real_y  sklearn_y   my_lin_y')
+    for i in range(n):
+        print(f'{x_test[i][0]:.2f}    {y_test[i]:.2f}  {sl_predictions[i]:.2f}   {my_lin_predictions[i]:.2f}')
+
+    #Отрисовка
+    plt.figure(figsize = (16,8))
+    plt.title('Linear regressions')
+    plt.xlabel('BMI')
+    plt.ylabel('Glucose level')
+    plt.scatter(x_train, y_train, color = 'black', label='Train values')
+    plt.scatter(x_test, y_test, label = 'Test values')
+
+    plt.axline((x_test[0][0], sl_predictions[0]), (x_test[1][0], sl_predictions[1]), label = 'sklearn regression', color = 'Red', linewidth = 3)
+    plt.axline((x_test[0][0], my_lin_predictions[0]), (x_test[1][0], my_lin_predictions[1]), label = 'my linear regression', color = 'Orange',linestyle = 'dashed' , linewidth = 2)
+
+
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
